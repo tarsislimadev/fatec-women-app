e70edcb8c561894b54a7ba13da6d4699e9dfaaab
Tarsis Lima <69215425+tarsislimadev@users.noreply.github.com>
2026-09-08 11:42:42 -0300

    Remove unused mobile project

--- src/mobile/lib/config/environment.dart
+++
@@ -1,9 +0,0 @@
-class Environment {
-  static const String apiUrl = String.fromEnvironment('API_URL', defaultValue: 'http://api:3000');
-
-  static const String apiVersion = '/api/v1';
-
-  static String get baseUrl => '$apiUrl$apiVersion';
-
-  static String appName() => String.fromEnvironment('APP_NAME', defaultValue: 'Fatec Register');
-}
